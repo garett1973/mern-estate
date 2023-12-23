e06946966fd82e9a44f0e197b7258c519b23c008
garett1973 <hobby@virgis.net>
2023-12-23 22:40:13 +0200

    creates and runs the server

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Virgis</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 flex rounded-lg">
+          <div className="flex items-center justify-between px-3 py-2 border border-slate-300 rounded w-24 sm:w-64">
+            <input
+              className="text-slate-700 bg-transparent rounded rounded-lg focus:outline-none"
+              type="text"
+              placeholder="Search..."
+            />
+            <FaSearch className="text-slate-500" />
+          </div>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className="hover:underline hover:cursor-pointer">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
