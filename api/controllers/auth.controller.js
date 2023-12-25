ad2164458e97a65b0c6618f9bd1c3af512762e35
garett1973 <hobby@virgis.net>
2023-12-25 11:04:48 +0200

    implements sign in API route with access_token saved in cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password, passwordConfirm } = req.body;
@@ -22,6 +23,42 @@ export const signup = async (req, res, next) => {
       message: "User created successfully",
       user: savedUser,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(false, 400, "Invalid credentials"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(false, 400, "Invalid credentials"));
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id, username: validUser.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    const { password: userPassword, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "User logged in successfully",
+        user: rest,
+      });
   } catch (err) {
     next(err);
   }
