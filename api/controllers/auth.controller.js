4fa92fac8c8557769e80440a7f648ba7dde36170
garett1973 <hobby@virgis.net>
2023-12-25 10:25:32 +0200

    Implements signup functionality with password confirmation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,18 +2,26 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, passwordConfirm } = req.body;
+
+  if (password !== passwordConfirm) {
+    return res.status(400).json({
+      success: false,
+      statusCode: 400,
+      message: "Passwords don't match",
+    });
+  }
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     const savedUser = await newUser.save();
-    res.status(201).json([
-      {
-        message: "User created successfully",
-        user: savedUser,
-      },
-    ]);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: savedUser,
+    });
   } catch (err) {
     next(err);
   }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,40 +1,89 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        body: JSON.stringify(formState),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (!data.success) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      } else {
+        setError(null);
+        setLoading(false);
+        navigate("/signin");
+      }
+    } catch (error) {
+      loading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-center text-3xl my-7 font-semibold">Sign Up</h1>
-      <form className="flex flex-col items-center">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
         <input
           className="w-full md:w-3/4 border-2 border-gray-400 rounded-lg px-3 py-2 my-2"
+          onChange={handleChange}
           id="username"
           type="text"
           placeholder="Username"
         />
         <input
           className="w-full md:w-3/4 border-2 border-gray-400 rounded-lg px-3 py-2 my-2"
+          onChange={handleChange}
           id="email"
           type="text"
           placeholder="Email"
         />
         <input
           className="w-full md:w-3/4 border-2 border-gray-400 rounded-lg px-3 py-2 my-2"
+          onChange={handleChange}
           id="password"
           type="password"
           placeholder="Password"
         />
         <input
           className="w-full md:w-3/4 border-2 border-gray-400 rounded-lg px-3 py-2 my-2"
-          id="confirmPassword"
+          onChange={handleChange}
+          id="passwordConfirm"
           type="password"
           placeholder="Confirm Password"
         />
         <button
+          disabled={loading}
           className="w-full md:w-3/4 uppercase border-2 border-gray-400 rounded-lg px-3 py-2 my-2 bg-gray-400 hover:bg-gray-500 text-white font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
           id="signUp_submit"
           type="submit"
         >
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div>
@@ -45,6 +94,7 @@ export default function SignUp() {
           </Link>
         </p>
       </div>
+      {error && <div className="text-center text-red-500 my-2">{error}</div>}
     </div>
   );
 }
