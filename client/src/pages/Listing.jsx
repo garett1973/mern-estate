cd8429a10c7ee42f5bfe9b5c92764a6cd1174ddd
garett1973 <hobby@virgis.net>
2024-01-30 15:32:56 +0200

    completes listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import { useSelector } from "react-redux";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -11,6 +20,7 @@ export default function Listing() {
   const [loading, setLoading] = useState(true); // [1]
   const [listingError, setListingError] = useState(false);
   const [listing, setListing] = useState(null);
+  const [copied, setCopied] = useState(false);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -65,6 +75,72 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${" "}
+              {listing.offer
+                ? listing.discountedPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600 my-2 text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listing.regularPrice - +listing.discountedPrice}
+                </p>
+              )}
+            </div>
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description</span> -{" "}
+              {listing.description}
+            </p>
+            <ul className="whitespace-nowrap text-green-800 font-semibold flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-2 ">
+                <FaBed className="text-lg" />
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} bedrooms`
+                  : "1 bedroom"}
+              </li>
+              <li className="flex items-center gap-2 ">
+                <FaBath className="text-lg" />
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} bathrooms`
+                  : "1 bathroom"}
+              </li>
+              <li className="flex items-center gap-2 ">
+                <FaParking className="text-lg" />
+                {listing.parking ? "Parking available" : "No parking"}
+              </li>
+              <li className="flex items-center gap-2 ">
+                <FaChair className="text-lg" />
+                {listing.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+          </div>
         </>
       )}
     </main>
